4d1d35fa7c99d729171ebae68820a3872adbbdfb
razmanika <nikoloz.razmadze.1@btu.edu.ge>
2020-06-29 15:37:52 +0400

    Change result

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -5,11 +5,11 @@ let result2;
 
 
 
-// result1 = income == strIncome; // mnishvnelobit igive
-// result2 = income === strIncome; // type it erti da igive
+result1 = income == strIncome; // mnishvnelobit igive
+result2 = income === strIncome; // type it erti da igive
 
-result1 = income != strIncome; // abrunebs False radgan 100 = "100" == amis shemtxvevashi
-result2 = income !== strIncome; // True radgan 100 gansxvavebuli tipisaa "100" sgan
+// result1 = income != strIncome; // abrunebs False radgan 100 = "100" == amis shemtxvevashi
+// result2 = income !== strIncome; // True radgan 100 gansxvavebuli tipisaa "100" sgan
 
 
 
